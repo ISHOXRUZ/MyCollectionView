472a2d0d53eb6b81c6fa96ff0a2fd07ef600dd41
ISHOXRUZ <48910512+ISHOXRUZ@users.noreply.github.com>
2020-02-19 21:23:58 -0800

    Initial Commit

---
+++ MyCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCollectionView
+//
+//  Created by Lucifer on 2/19/20.
+//  Copyright © 2020 Lucifer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
