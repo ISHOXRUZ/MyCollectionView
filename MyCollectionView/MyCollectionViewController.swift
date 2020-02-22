19da8850070ffec998d3f7709e6456c1589d2190
ISHOXRUZ <48910512+ISHOXRUZ@users.noreply.github.com>
2020-02-21 23:18:36 -0800

    Update MyCollectionViewController.swift

--- MyCollectionView/MyCollectionViewController.swift
+++ MyCollectionView/MyCollectionViewController.swift
@@ -12,18 +12,15 @@ private let reuseIdentifier = "ArtCover"
 
 class MyCollectionViewController: UICollectionViewController {
     
+    let imageNameArray = ["balkan.jpg", "Bochka.jpg", "Bonsai", "dastarhan.jpg", "elu.jpg", "indokitay.jpg", "istorii", "klassik.jpg", "love.jpg", "morris.jpg", "ogonek.jpg", "respublika.jpg", "shok.jpg", "speakeasy.jpg", "x.o.jpg"
+    ]
+    
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
 
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
     }
 
     /*
@@ -41,46 +38,17 @@ class MyCollectionViewController: UICollectionViewController {
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return imageNameArray.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ArtCoverCell
     
+        cell.coverImageView.image = UIImage(named: imageNameArray[indexPath.row])
         // Configure the cell
     
         return cell
     }
 
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
 
 }
